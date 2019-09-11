68c926e997a08284989997a0f6aef943f575eba7
Daniel Johansson <johansson.gd@gmail.com>
2019-09-11 20:23:29 +0200

    Lagt till databasanslutning.

--- ApiTest/ApiTest/Data/ApplicationDbContext.cs
+++ ApiTest/ApiTest/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ApiTest.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace ApiTest.Data
             : base(options)
         {
         }
+
+        public DbSet<Person> People { get; set; }
     }
 }
