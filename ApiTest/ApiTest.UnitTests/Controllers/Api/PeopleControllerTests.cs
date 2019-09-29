24ecd5461582725a9c94d865e37a0a057659c490
Daniel Johansson <johansson.gd@gmail.com>
2019-09-29 15:32:09 +0200

    Börjat med tester

---
+++ ApiTest/ApiTest.UnitTests/Controllers/Api/PeopleControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using ApiTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NUnit.Framework;
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using ApiTest.Data;
+using ApiTest.Controllers.Api;
+
+namespace ApiTest.UnitTests.Controllers.Api
+{
+    [TestFixture]
+    class PeopleControllerTests
+    {
+        [Test]
+        public void GetPeople_WhenCalled_ReturnListOfPeople()
+        {
+            var context = new Mock<ApplicationDbContext>();
+            context.Setup(c => c.People.ToList()).Returns(new List<Person>
+            {
+                new Person() { Id = 1, Firstname = "Torsten", Lastname = "Torstensson", Email = "torsten@gmail.com", City = "Berlin", DateCreated = DateTime.Now},
+                new Person() { Id = 2, Firstname = "Uno", Lastname = "Unosson", Email = "uno@gmail.com", City = "Amsterdam", DateCreated = DateTime.Now}
+            });
+
+            var controller = new PeopleController(context.Object, new Mapper());
+        }
+    }
+}
